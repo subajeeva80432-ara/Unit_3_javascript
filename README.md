1817aca8d9c55f18205654f6ac8ba2e2f997b709
Subasri.A <subajeeva80432@gmail.com>
2026-04-15 14:05:42 +0530

    Final upload all 15 tasks

---
+++ README.md
@@ -0,0 +1,3 @@
+# JavaScript-Fundamentals-Lab
+This repository contains JavaScript Unit 3 lab tasks including syntax, variables, data types, operators, conditionals, loops, functions, arrays, objects, DOM manipulation, and event handling with examples.
+# Notes 
---
+++ task 1/syntax&variables.js
@@ -0,0 +1,16 @@
+// JavaScript Variables Example
+
+var a = 10;
+let b = 20;
+const c = 30;
+
+console.log("var:", a);
+console.log("let:", b);
+console.log("const:", c);
+
+// Dynamic typing
+let value = 100;
+console.log(value);
+
+value = "Now I am string";
+console.log(value);
